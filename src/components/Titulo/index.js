6d2d5b3aa39fea0a290a34ae74005b4926b48e2e
NachoH <98nachohuerta@gmail.com>
2020-11-14 18:24:18 -0300

    Commit inicial del proyecto

---
+++ src/components/Titulo/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Title} from '../Titulo/styles'
+
+const Titulo = () => {
+
+    return (  
+        <>
+            <Title><h1>Hola</h1></Title>
+        </>
+    );
+}
+ 
+export default Titulo;
